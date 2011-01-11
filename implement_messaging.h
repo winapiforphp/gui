d95cc51ba3c08ec83edfa9915a76859430b4f9b0
Mark Skilbeck <devnull@localhost>
2011-01-11 19:48:34 +0000

    (c) 2010 -> 2011

--- implement_messaging.h
+++ implement_messaging.h
@@ -2,7 +2,7 @@
   +----------------------------------------------------------------------+
   | PHP Version 5                                                        |
   +----------------------------------------------------------------------+
-  | Copyright (c) 2007-2008 Elizabeth M. Smith, Sara Golemon, Tom Rogers |
+  | Copyright (c) 2007-2011 Elizabeth M. Smith, Sara Golemon, Tom Rogers |
   +----------------------------------------------------------------------+
   | This source file is subject to version 3.01 of the PHP license,      |
   | that is bundled with this package in the file LICENSE, and is        |
